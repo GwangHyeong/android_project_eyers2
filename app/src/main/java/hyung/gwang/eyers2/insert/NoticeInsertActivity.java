0e8c1ff3d22452d950f764e769f6ba6eda9f9cff
GwangHyeong <rhkdgud61@naver.com>
2020-04-30 02:27:26 +0900

    4/30

---
+++ app/src/main/java/hyung/gwang/eyers2/insert/NoticeInsertActivity.java
@@ -0,0 +1,17 @@
+package hyung.gwang.eyers2.insert;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import hyung.gwang.eyers2.R;
+
+public class NoticeInsertActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        setContentView(R.layout.activity_scanid);
+
+    }
+}
