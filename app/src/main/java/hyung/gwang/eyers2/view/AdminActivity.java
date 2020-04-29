c718e365759429844782320bdd2737210c6aa465
GwangHyeong <rhkdgud61@naver.com>
2020-04-29 17:01:38 +0900

    아이어즈 커뮤니티 앱 개발중

--- app/src/main/java/hyung/gwang/eyers2/view/AdminActivity.java
+++ app/src/main/java/hyung/gwang/eyers2/view/AdminActivity.java
@@ -1,14 +1,26 @@
 package hyung.gwang.eyers2.view;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import hyung.gwang.eyers2.R;
 
 public class AdminActivity  extends AppCompatActivity {
+    /**상단 선언부**/
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
+
+        button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
